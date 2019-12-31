43eaada5da7ba6671d2fbd59ba89a20e12ef8604
Dash Bell <dbell@btr.pm>
2019-12-31 17:51:31 +0100

    Updates

---
+++ precache-manifest.5b376a08bf91c825484d6f27c0e74066.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf19c4e559b0b374d502849d8e7618b",
+    "url": "/boarding-pass-scanner/index.html"
+  },
+  {
+    "revision": "b18d4715f8e4e1f84d1c",
+    "url": "/boarding-pass-scanner/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f162e5636527fdfe6dae",
+    "url": "/boarding-pass-scanner/static/js/2.c1a9b7ef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/boarding-pass-scanner/static/js/2.c1a9b7ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b18d4715f8e4e1f84d1c",
+    "url": "/boarding-pass-scanner/static/js/main.84802fb6.chunk.js"
+  },
+  {
+    "revision": "0ff623dcfadf180494b3",
+    "url": "/boarding-pass-scanner/static/js/runtime-main.ae57ce1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/boarding-pass-scanner/static/media/logo.5d5d9eef.svg"
+  }
+]);
